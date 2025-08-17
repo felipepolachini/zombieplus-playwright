cb23345056420fc1388d3e24019d155ff7208a98
Felipe Polachini <fpiandoli@gmail.com>
2025-08-17 19:33:06 -0300

    fix bug

--- tests/support/index.js
+++ tests/support/index.js
@@ -5,15 +5,17 @@ const {Toast} = require('../pages/Components');
 const {MoviesPage} = require('../pages/MoviesPage');
 const {LandingPage} = require('../pages/LandingPage');
 
+
 const test = base.extend({
     page: async ({page},use) =>{
-        await use({
-            ...page,
-            login: new LoginPage(page),
-            toast: new Toast(page),
-            movies: new MoviesPage(page),
-            landing: new LandingPage(page)
-        })
+        const context = page
+
+        context['landing'] = new LandingPage(page)
+        context['login'] = new LoginPage(page)
+        context['movies'] = new MoviesPage(page)
+        context['toast'] = new Toast(page)
+        
+        await use(context)
     }
 })
 
